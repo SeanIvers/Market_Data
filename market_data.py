171dbb3c9a3d5968d47c856d66dda087efcda99d
SeanIvers <seanivers55@gmail.com>
2022-02-06 00:46:25 -0500

    IBapi class added

--- market_data.py
+++ market_data.py
@@ -9,3 +9,11 @@ import pandas as pd
 import plotly.graph_objects as go
 import streamlit as st
 
+class IBapi(EWrapper, EClient):
+    def __init__(self):
+        EClient.__init__(self, self)
+        self.data = []
+
+    def historicalData(self, bar):
+        self.data.append([bar.date, bar.open, bar.high, bar.low, bar.close])
+
